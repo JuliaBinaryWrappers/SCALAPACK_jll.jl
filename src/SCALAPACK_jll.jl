20eea64bb7b9c697dcf90fd4afe3a62c80dcb51b
jlbuild <juliabuildbot@gmail.com>
2026-03-20 15:10:59 +0000

    SCALAPACK_jll build 2.2.3+0

--- src/SCALAPACK_jll.jl
+++ src/SCALAPACK_jll.jl
@@ -7,5 +7,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("SCALAPACK")
-JLLWrappers.@generate_main_file("SCALAPACK", UUID("5d3fc3e8-a677-5550-826f-6cfd58f208da"))
+JLLWrappers.@generate_main_file("SCALAPACK", Base.UUID("5d3fc3e8-a677-5550-826f-6cfd58f208da"))
 end  # module SCALAPACK_jll
